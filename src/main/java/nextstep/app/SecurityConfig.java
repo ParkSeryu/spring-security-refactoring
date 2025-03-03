57dbc48153956bc9b58345fb1e344fdc597cafb7
parkSeryu <pjh971212@gmail.com>
2025-03-03 13:45:41 +0900

    feat: OAuth AutoConfiguration 추가

--- src/main/java/nextstep/app/SecurityConfig.java
+++ src/main/java/nextstep/app/SecurityConfig.java
@@ -1,30 +1,17 @@
 package nextstep.app;
 
 import nextstep.oauth2.OAuth2ClientProperties;
-import nextstep.oauth2.authentication.OAuth2LoginAuthenticationProvider;
 import nextstep.oauth2.registration.ClientRegistration;
 import nextstep.oauth2.registration.ClientRegistrationRepository;
-import nextstep.oauth2.userinfo.OAuth2UserService;
-import nextstep.oauth2.web.OAuth2AuthorizationRequestRedirectFilter;
-import nextstep.oauth2.web.OAuth2AuthorizedClientRepository;
-import nextstep.oauth2.web.OAuth2LoginAuthenticationFilter;
 import nextstep.security.access.AnyRequestMatcher;
 import nextstep.security.access.MvcRequestMatcher;
-import nextstep.security.access.RequestMatcherEntry;
 import nextstep.security.access.hierarchicalroles.RoleHierarchy;
 import nextstep.security.access.hierarchicalroles.RoleHierarchyImpl;
-import nextstep.security.authentication.*;
 import nextstep.security.authorization.*;
 import nextstep.security.config.Customizer;
-import nextstep.security.config.DefaultSecurityFilterChain;
-import nextstep.security.config.DelegatingFilterProxy;
-import nextstep.security.config.FilterChainProxy;
 import nextstep.security.config.SecurityFilterChain;
 import nextstep.security.config.annotation.EnableWebSecurity;
 import nextstep.security.config.annotation.web.HttpSecurity;
-import nextstep.security.context.SecurityContextHolderFilter;
-import nextstep.security.userdetails.UserDetailsService;
-import nextstep.security.web.csrf.CsrfFilter;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -33,18 +20,11 @@ import org.springframework.http.HttpMethod;
 
 import java.util.*;
 
-
 @Configuration
 @EnableAspectJAutoProxy
 @EnableConfigurationProperties(OAuth2ClientProperties.class)
 @EnableWebSecurity
 public class SecurityConfig {
-    private final OAuth2ClientProperties oAuth2ClientProperties;
-
-    public SecurityConfig(OAuth2ClientProperties oAuth2ClientProperties) {
-        this.oAuth2ClientProperties = oAuth2ClientProperties;
-    }
-
     @Bean
     public SecuredMethodInterceptor securedMethodInterceptor() {
         return new SecuredMethodInterceptor();
@@ -72,27 +52,6 @@ public class SecurityConfig {
                         .requestsMatchers(AnyRequestMatcher.INSTANCE, new PermitAllAuthorizationManager<Void>())
                 )
                 .build();
-    }
-
-    @Bean
-    public ClientRegistrationRepository clientRegistrationRepository() {
-        Map<String, ClientRegistration> registrations = getClientRegistrations(oAuth2ClientProperties);
-        return new ClientRegistrationRepository(registrations);
-    }
-
-    private static Map<String, ClientRegistration> getClientRegistrations(OAuth2ClientProperties properties) {
-        Map<String, ClientRegistration> clientRegistrations = new HashMap<>();
-        properties.getRegistration().forEach((key, value) -> clientRegistrations.put(key,
-                getClientRegistration(key, value, properties.getProvider().get(key))));
-        return clientRegistrations;
-    }
-
-    private static ClientRegistration getClientRegistration(String registrationId,
-                                                            OAuth2ClientProperties.Registration registration,
-                                                            OAuth2ClientProperties.Provider provider) {
-        return new ClientRegistration(registrationId, registration.getClientId(), registration.getClientSecret(),
-                registration.getRedirectUri(), registration.getScope(), provider.getAuthorizationUri(),
-                provider.getTokenUri(), provider.getUserInfoUri(), provider.getUserNameAttributeName());
     }
 }
 
